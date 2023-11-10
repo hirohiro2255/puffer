30ac19cc433d3303add69d978e88e9d62dcb8a0d
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-11-10 21:27:22 +0900

    Added tests for function to parse fen string

--- test_app.py
+++ test_app.py
@@ -1,9 +1,87 @@
 import unittest
-from app import Chess
+from app import Chess, board_from_fen
 from defs import is_white, is_black, WHITE, BLACK, KNIGHT, BISHOP, ROOK, QUEEN, KING, PAWN, is_pawn, is_knight, is_bishop, is_rook, is_queen, is_king, is_empty, is_outside_board, EMPTY, SENTINEL
 
 
 class TestChessClass(unittest.TestCase):
+    def test_empty_board(self):
+        b = board_from_fen("8/8/8/8/8/8/8/8 w KQkq - 0 1")
+        for i in range(2, 10):
+            for j in range(2, 10):
+                self.assertEqual(b.state[i][j], EMPTY)
+
+    def test_start_pos(self):
+        b = board_from_fen()
+        self.assertEqual(b.state[2][2], BLACK | ROOK)
+        self.assertEqual(b.state[2][3], BLACK | KNIGHT)
+        self.assertEqual(b.state[2][4], BLACK | BISHOP)
+        self.assertEqual(b.state[2][5], BLACK | QUEEN)
+        self.assertEqual(b.state[2][6], BLACK | KING)
+        self.assertEqual(b.state[2][7], BLACK | BISHOP)
+        self.assertEqual(b.state[2][8], BLACK | KNIGHT)
+        self.assertEqual(b.state[2][9], BLACK | ROOK)
+
+        for i in range(2, 10):
+            self.assertEqual(b.state[3][i], BLACK | PAWN)
+
+        for i in range(4, 8):
+            for j in range(2, 10):
+                self.assertEqual(b.state[i][j], EMPTY)
+
+        self.assertEqual(b.state[9][2], WHITE | ROOK)
+        self.assertEqual(b.state[9][3], WHITE | KNIGHT)
+        self.assertEqual(b.state[9][4], WHITE | BISHOP)
+        self.assertEqual(b.state[9][5], WHITE | QUEEN)
+        self.assertEqual(b.state[9][6], WHITE | KING)
+        self.assertEqual(b.state[9][7], WHITE | BISHOP)
+        self.assertEqual(b.state[9][8], WHITE | KNIGHT)
+        self.assertEqual(b.state[9][9], WHITE | ROOK)
+
+        for i in range(2, 10):
+            self.assertEqual(b.state[8][i], WHITE | PAWN)
+
+    def test_correct_starting_player(self):
+        b = board_from_fen(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        self.assertEqual(b.to_move, WHITE)
+        b = board_from_fen(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1")
+        self.assertEqual(b.to_move, BLACK)
+
+    def test_random_pos(self):
+        b = board_from_fen(
+            "4R1B1/1kp5/1B1Q4/1P5p/1p2p1pK/8/3pP3/4N1b1 w - - 0 1")
+        self.assertEqual(b.state[2][6], WHITE | ROOK)
+        self.assertEqual(b.state[2][8], WHITE | BISHOP)
+        self.assertEqual(b.state[3][3], BLACK | KING)
+        self.assertEqual(b.state[3][4], BLACK | PAWN)
+        self.assertEqual(b.state[4][3], WHITE | BISHOP)
+        self.assertEqual(b.state[4][5], WHITE | QUEEN)
+        self.assertEqual(b.state[5][3], WHITE | PAWN)
+        self.assertEqual(b.state[5][9], BLACK | PAWN)
+        self.assertEqual(b.state[6][3], BLACK | PAWN)
+        self.assertEqual(b.state[6][6], BLACK | PAWN)
+        self.assertEqual(b.state[6][8], BLACK | PAWN)
+        self.assertEqual(b.state[6][9], WHITE | KING)
+        self.assertEqual(b.state[8][5], BLACK | PAWN)
+        self.assertEqual(b.state[8][6], WHITE | PAWN)
+        self.assertEqual(b.state[9][6], WHITE | KNIGHT)
+        self.assertEqual(b.state[9][8], BLACK | BISHOP)
+
+    def test_bad_fen_string(self):
+        with self.assertRaises(ValueError):
+            b = board_from_fen('this is bad string')
+
+    def test_bad_fen_string_bad_char(self):
+        with self.assertRaises(ValueError):
+            b = board_from_fen(
+                "rnbqkbnH/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
+    def test_bad_fen_string_too_many_chars(self):
+        with self.assertRaises(IndexError):
+            b = board_from_fen(
+                "rnbqkbnrrrrr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+
     def test_piece_recognized(self):
         self.assertTrue(is_white(WHITE | BISHOP))
         self.assertTrue(is_white(WHITE | ROOK))
